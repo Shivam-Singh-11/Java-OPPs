600695a991919e43656a2a62dc38505ff9433058
Shivam-Singh-11 <shivamis555555@gmail.com>
2026-09-11 16:11:17 +0530

    Add Java OOP programs

---
+++ AgeValidation.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+public class AgeValidation {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int age = sc.nextInt();
+
+        try {
+            ageCheck(age);
+        }
+        catch (InvalidAgeException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    static void ageCheck(int a) throws InvalidAgeException {
+
+        if (a < 18) {
+            throw new InvalidAgeException("Age is Invalid");
+        }
+
+        System.out.println("eligible to vote");
+    }
+}
+class InvalidAgeException extends Exception {
+
+    InvalidAgeException(String msg) {
+        super(msg);
+    }
+}
